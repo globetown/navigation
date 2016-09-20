facef871709cc2982183182626ce364db986939a
Besart Hoxhaj <besartshyti@gmail.com>
2016-09-21 00:15:42 +0100

    implement navigation

--- index.ios.js
+++ index.ios.js
@@ -6,48 +6,137 @@
 
 import React, { Component } from 'react';
 import {
+  NavigationExperimental,
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  PixelRatio,
+  ScrollView,
+  TouchableHighlight
 } from 'react-native';
 
-class Navigation extends Component {
-  render() {
+const {
+  CardStack:NavigationCardStack,
+  StateUtils:NavigationStateUtils
+} = NavigationExperimental;
+
+class BleedingEdgeApplication extends Component {
+  constructor(props,context){
+    super(props,context);
+    this.state = {navigationState:{index:0,routes:[{key:'My Initial Scene'}]}};
+    this._onNavigationChange = this._onNavigationChange.bind(this);
+  }
+  _onNavigationChange(type){
+    let {navigationState} = this.state;
+    switch (type) {
+    case 'push':
+      const route = {key:'Route-'+ Date.now()};
+      navigationState = NavigationStateUtils.push(navigationState,route);
+      break;
+    case 'pop':
+      navigationState = NavigationStateUtils.pop(navigationState);
+      break;
+    }
+    if (this.state.navigationState !== navigationState) {
+      this.setState({navigationState});
+    }
+  }
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <MyVerySimpleNavigator
+        navigationState={this.state.navigationState}
+        onNavigationChange={this._onNavigationChange}
+        onExit={this._exit}
+      />
+    );
+  }
+}
+
+class TappableRow extends Component {
+  render(){
+    return (
+      <TouchableHighlight
+        style={styles.row}
+        underlayColor='#D0D0D0'
+        onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>
+          {this.props.text}
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+      </TouchableHighlight>
+    );
+  }
+}
+
+class MyVeryComplexScene extends Component {
+  render(){
+    return (
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.row}>
+          Route: {this.props.route.key}
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <TappableRow
+          text='Tap me to load the next scene'
+          onPress={this.props.onPushRoute}
+        />
+        <TappableRow
+          text='Tap me to go back'
+          onPress={this.props.onPopRoute}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+class MyVerySimpleNavigator extends Component {
+  constructor(props,context) {
+    super(props,context);
+    this._onPushRoute = this.props.onNavigationChange.bind(null,'push');
+    this._onPopRoute = this.props.onNavigationChange.bind(null,'pop');
+    this._renderScene = this._renderScene.bind(this);
+  }
+  _renderScene(sceneProps){
+    return (
+      <MyVeryComplexScene
+      route={sceneProps.scene.route}
+      onPushRoute={this._onPushRoute}
+      onPopRoute={this._onPopRoute}
+      onExit={this.props.onExit}
+      />
+    );
+  }
+  render(){
+    return (
+      <NavigationCardStack
+        onNavigateBack={this._onPopRoute}
+        navigationState={this.props.navigationState}
+        renderScene={this._renderScene}
+        style={styles.navigator}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  navigator:{
+    flex:1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  scrollView:{
+    marginTop:64
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  row:{
+    padding:15,
+    backgroundColor:'white',
+    borderBottomWidth:1 / PixelRatio.get(),
+    borderBottomColor:'#CDCDCD'
   },
+  rowText:{
+    fontSize:17
+  },
+  buttonText:{
+    fontSize:17,
+    fontWeight:'500'
+  }
 });
 
-AppRegistry.registerComponent('Navigation', () => Navigation);
+AppRegistry.registerComponent('Navigation',() => BleedingEdgeApplication);
